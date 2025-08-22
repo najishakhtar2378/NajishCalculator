4ddb1455a0119ccf0f5f54ccf12fcfd3e1fdf72f
NAJISH  AKHTAR <166438726+najishakhtar2378@users.noreply.github.com>
2025-08-23 01:43:53 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// import './App.css';
+import { useState } from "react";
+import styles from "./App.module.css";
+import ButtonsContainer from "./ButtonsContainer";
+import Display from "./Display";
+function App() {
+  const [calVal,setCalVal]=  useState();
+  const onButtonClick=(buttonText)=>{
+    if(buttonText==="C"){
+      setCalVal("")
+      
+    }else if(buttonText==="="){
+      const result=eval(calVal)
+      setCalVal(result)
+    }else{
+      const newDisplayValue=calVal+buttonText;
+      setCalVal(newDisplayValue)
+    }
+  }
+  return (
+    <div className={styles.calculator}>
+      <Display displayValue={calVal}></Display>
+      <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
+    </div>
+  );
+
+  
+}
+
+export default App
